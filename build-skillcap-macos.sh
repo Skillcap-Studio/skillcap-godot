e2c8e637d9388a5d9b8893283a3fb464dda1b5b1
Rafael Vidaurre <rafael@vidaurre.io>
2023-12-22 09:31:38 +0000

    feat: add debug symbols to scons flags

--- build-skillcap-macos.sh
+++ build-skillcap-macos.sh
@@ -5,10 +5,10 @@ set -e
 GODOT_VERSION=$(python -c "from version import *; print(str(major) + '.' + str(minor) + '.' + str(patch))")
 
 # Compile for Intel (x86-64) powered Macs
-scons platform=macos arch=x86_64 module_text_server_fb_enabled=yes
+scons platform=macos arch=x86_64 module_text_server_fb_enabled=yes debug_symbols=yes
 
 # Compile for Apple Silicon (ARM64) powered Macs
-scons platform=macos arch=arm64 module_text_server_fb_enabled=yes
+scons platform=macos arch=arm64 module_text_server_fb_enabled=yes debug_symbols=yes
 
 # Bundle both architectures into a single "Universal 2" binary
 lipo -create bin/godot.macos.editor.x86_64 bin/godot.macos.editor.arm64 -output bin/godot.macos.editor.universal
